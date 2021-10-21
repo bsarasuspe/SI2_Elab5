302138674db0407aeb783b402becc30c1d139694
EHU <a@a.com>
2021-10-21 12:16:43 +0200

    bigarren atala hasita

--- src/SRP/Bill.java
+++ src/SRP/Bill.java
@@ -1,5 +1,27 @@
 package SRP;
 
-public class Bill {
+import java.util.Date;
 
+public class Bill {
+	public String code;
+	public Date billDate;
+	public float billAmount;
+	public float VAT;
+	public float billDeduction;
+	public float billTotal;
+	public int deductionPercentage;
+	
+	// Fakturaren totala kalkulatzen duen metodoa.
+	public void totalCalc() {
+	// Dedukzioa kalkulatu
+	billDeduction = (billAmount * deductionPercentage) / 100;
+	// VAT kalkulatzen dugu
+	VAT = (float) (billAmount * 0.16);
+	// Totala kalkulatzen dugu
+	billTotal = (billAmount - billDeduction) + VAT;
+	}
+	/*
+	public double getBillDeduction() {
+		
+	}*/
 }
